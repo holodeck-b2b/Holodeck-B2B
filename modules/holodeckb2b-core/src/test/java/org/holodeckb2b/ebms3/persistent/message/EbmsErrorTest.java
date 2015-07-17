35ec2acfd53a7f9f2771a64ea7c5d46f9cb82eac
Sander Fieten <sander@holodeck-b2b.org>
2015-07-17 18:35:39 +0200

    Corrected test for extended length of error fields

--- modules/holodeckb2b-core/src/test/java/org/holodeckb2b/ebms3/persistent/message/EbmsErrorTest.java
+++ modules/holodeckb2b-core/src/test/java/org/holodeckb2b/ebms3/persistent/message/EbmsErrorTest.java
@@ -46,10 +46,79 @@ public class EbmsErrorTest {
     private static final String T_ERROR_CODE = "error-code";
     private static final IEbmsError.Severity T_SEVERITY = IEbmsError.Severity.FAILURE;
     private static final String T_MESSAGE = "error-short-description";
+    
     private static final String T_ERROR_DETAIL = "error-detail";
+    private static final String T_LARGE_ERROR_DETAIL = "com.flame.shared.exceptions.PackingException: Message verification,\n" +
+"                    authorisation, or decryption fail ed: com.sun.xml.wss.XWSSecurityException:\n" +
+"                    javax.xml.crypto.MarshalException: java.security.NoSuchAlgorithmException: no\n" +
+"                    such algorithm:\n" +
+"                    http://docs.oasis-open.org/wss/oasis-wss-SwAProfile-1.1#Attachment-C ontent- be8\n" +
+"                    Signature-Transform for provider XMLDSig at\n" +
+"                    com.flame.connection.impl.ebXML.AS4.Session.run(Session.java:630) at\n" +
+"                    java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:471) at\n" +
+"                    java.util.concurrent.FutureTask.run(FutureTask.java:262) at\n" +
+"                    java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1145)\n" +
+"                    at\n" +
+"                    java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:615)\n" +
+"                    at java.lang.Thread.run(Thread.java:745) Caused by:\n" +
+"                    com.sun.xml.wss.XWSSecurityException: com.sun.xml.wss.XWSSecurityException:\n" +
+"                    javax.xml.crypto.MarshalException: java.security.NoSuchAlgorithmException: no\n" +
+"                    such algorithm:\n" +
+"                    http://docs.oasis-open.org/wss/oasis-wss-SwAProfile-1.1#Attachment-Content- be8\n" +
+"                    Signature-Transform for provider XMLDSig at\n" +
+"                    com.sun.xml.wss.impl.misc.XWSSProcessor2_0Impl.verifyInboundMessage(XWSSProcessor2_0Impl.java:158)\n" +
+"                    at\n" +
+"                    com.flame.packaging.wrapping.ebXML.AS4.PackageManager.verifyAndDecrypt(PackageManager.java:1419)\n" +
+"                    at\n" +
+"                    com.flame.packaging.wrapping.ebXML.AS4.PackageManager.securityPass(PackageManager.java:2272)\n" +
+"                    at com.flame.connection.impl.ebXML.AS4.Session.run(Session.java:609) ... 5 more\n" +
+"                    Caused by: com.sun.xml.wss.XWSSecurityException:\n" +
+"                    javax.xml.crypto.MarshalException: java.security.NoSuchAlgorithmException: no\n" +
+"                    such algorithm:\n" +
+"                    http://docs.oasis-open.org/wss/oasis-wss-SwAProfile-1.1#Attachment-Content- be8\n" +
+"                    Signature-Transform for provider XMLDSig at\n" +
+"                    com.sun.xml.wss.impl.dsig.SignatureProcessor.verify(SignatureProcessor.java:916)\n" +
+"                    at com.sun.xml.wss.impl.filter.SignatureFilter.process(SignatureFilter.java:638)\n" +
+"                    at\n" +
+"                    com.sun.xml.wss.impl.SecurityRecipient.pProcessOnce(SecurityRecipient.java:1199)\n" +
+"                    at com.sun.xml.wss.impl.SecurityRecipient.pProcess(SecurityRecipient.java:1283)\n" +
+"                    at\n" +
+"                    com.sun.xml.wss.impl.SecurityRecipient.processMessagePolicy(SecurityRecipient.java:782)\n" +
+"                    at\n" +
+"                    com.sun.xml.wss.impl.SecurityRecipient.validateMessage(SecurityRecipient.java:261)\n" +
+"                    at\n" +
+"                    com.sun.xml.wss.impl.misc.XWSSProcessor2_0Impl.verifyInboundMessage(XWSSProcessor2_0Impl.java:156)\n" +
+"                    ... 8 more Caused by: javax.xml.crypto.MarshalException:\n" +
+"                    java.security.NoSuchAlgorithmException: no such algorithm:\n" +
+"                    http://docs.oasis-open.org/wss/oasis-wss-SwAProfile-1.1#Attachment-Content- be8\n" +
+"                    Signature-Transform for provider XMLDSig at\n" +
+"                    org.jcp.xml.dsig.internal.dom.DOMTransform.&lt;init>(DOMTransform.java:80) at\n" +
+"                    org.jcp.xml.dsig.internal.dom.DOMReference.&lt;init>(DOMReference.java:204) at\n" +
+"                    org.jcp.xml.dsig.internal.dom.DOMSignedInfo.&lt;init>(DOMSignedInfo.java:177) at\n" +
+"                    org.jcp.xml.dsig.internal.dom.DOMXMLSignature.&lt;init>(DOMXMLSignature.java:141)\n" +
+"                    at\n" +
+"                    org.jcp.xml.dsig.internal.dom.DOMXMLSignatureFactory.unmarshal(DOMXMLSignatureFactory.java:173)\n" +
+"                    at\n" +
+"                    org.jcp.xml.dsig.internal.dom.DOMXMLSignatureFactory.unmarshalXMLSignature(DOMXMLSignatureFactory.java:137)\n" +
+"                    at\n" +
+"                    com.sun.xml.wss.impl.dsig.SignatureProcessor.verify(SignatureProcessor.java:775)\n" +
+"                    ... 14 more Caused by: java.security.NoSuchAlgorithmException: no such\n" +
+"                    algorithm:\n" +
+"                    http://docs.oasis-open.org/wss/oasis-wss-SwAProfile-1.1#Attachment-Content- be8\n" +
+"                    Signature-Transform for provider XMLDSig at\n" +
+"                    sun.security.jca.GetInstance.getService(GetInstance.java:100) at\n" +
+"                    javax.xml.crypto.dsig.TransformService.getInstance(TransformService.java:210) at\n" +
+"                    org.jcp.xml.dsig.internal.dom.DOMTransform.&lt;init>(DOMTransform.java:78) ...\n" +
+"                    20 more";
+    
     private static final String T_ORIGIN = "test-origin";
     
     private static final Description T_DESCRIPTION = new Description("Lorem ipsum dolor sit amet", "fake");
+    private static final Description T_LARGE_DESCRIPTION = new Description("Message verification, authorisation, or decryption\n" +
+"                    failed: com.sun.xml.wss.XWSSecurityException: javax.xml.crypto.MarshalException:\n" +
+"                    java.security.NoSuchAlgorithmException: no such algorithm:\n" +
+"                    http://docs.oasis-open.org/wss/oasis-wss-SwAProfil e-1.1#Attachment-Content- be8\n" +
+"                    Signature-Transform for provider XMLDSig", "fake");
     
     EntityManager       em;
 
@@ -272,4 +341,46 @@ public class EbmsErrorTest {
         em.getTransaction().commit();  
     }
     
+    @Test
+    public void test17_LargeDescription() {
+        em.getTransaction().begin();
+        EError instance = new EError();
+       
+        instance.eError.setDescription(T_LARGE_DESCRIPTION);
+        
+        em.persist(instance);        
+        em.getTransaction().commit();        
+        
+        em.getTransaction().begin();
+        List<EError> tps = em.createQuery("from EError", EError.class).getResultList();
+        
+        assertTrue(tps.size() == 1);
+        assertEquals(T_LARGE_DESCRIPTION.getLanguage(), tps.get(0).eError.getDescription().getLanguage());
+        assertEquals(T_LARGE_DESCRIPTION.getText(), tps.get(0).eError.getDescription().getText());
+        
+        em.remove(tps.get(0));
+        
+        em.getTransaction().commit();         
+    }
+    
+    @Test
+    public void test18_LargeErrorDetail() {
+        em.getTransaction().begin();
+        EError instance = new EError();
+       
+        instance.eError.setErrorDetail(T_LARGE_ERROR_DETAIL);
+        
+        em.persist(instance);        
+        em.getTransaction().commit();        
+        
+        em.getTransaction().begin();
+        List<EError> tps = em.createQuery("from EError", EError.class).getResultList();
+        
+        assertTrue(tps.size() == 1);
+        assertEquals(T_LARGE_ERROR_DETAIL, tps.get(0).eError.getErrorDetail());
+        
+        em.remove(tps.get(0));
+        
+        em.getTransaction().commit();         
+    }
 }
