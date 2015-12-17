4da5f1c841a161c3c7bbe5f8a86c7e8bb9d53361
Sander Fieten <sander@holodeck-b2b.org>
2015-12-17 22:42:25 +0100

    Removed redundant license header

--- modules/interfaces/src/main/java/org/holodeckb2b/interfaces/general/IService.java
+++ modules/interfaces/src/main/java/org/holodeckb2b/interfaces/general/IService.java
@@ -16,28 +16,6 @@
  */
 package org.holodeckb2b.interfaces.general;
 
-/*
- * #%L
- * Holodeck B2B - Interfaces
- * %%
- * Copyright (C) 2015 The Holodeck B2B Team
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/gpl-3.0.html>.
- * #L%
- */
-
 /**
  * Represents a <i>business service</i>, i.e. a service of the business application, that will process a message. 
  * Holodeck B2B itself will not use this information to determine message processing other than that it might be used 
--- modules/interfaces/src/main/java/org/holodeckb2b/interfaces/pmode/security/IUsernameTokenConfiguration.java
+++ modules/interfaces/src/main/java/org/holodeckb2b/interfaces/pmode/security/IUsernameTokenConfiguration.java
@@ -16,28 +16,6 @@
  */
 package org.holodeckb2b.interfaces.pmode.security;
 
-/*
- * #%L
- * Holodeck B2B - Interfaces
- * %%
- * Copyright (C) 2015 The Holodeck B2B Team
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/gpl-3.0.html>.
- * #L%
- */
-
 /**
  * Defines the configuration of a WSS UsernameToken contained in the security header of the ebMS message. Depending on
  * the direction (incoming or outgoing) of the message the information is used to set or validate the UsernameToken in
