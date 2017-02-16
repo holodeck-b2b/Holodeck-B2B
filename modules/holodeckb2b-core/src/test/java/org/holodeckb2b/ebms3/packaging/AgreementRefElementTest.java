3d98a80de2e89a0c4531d2c0585d0787979909a5
t.shakuov <t.shakuov@gmail.com>
2017-02-16 14:53:39 +0300

    AgreementRefElement junit test added.

---
+++ modules/holodeckb2b-core/src/test/java/org/holodeckb2b/ebms3/packaging/AgreementRefElementTest.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2016 The Holodeck B2B Team, Sander Fieten
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.holodeckb2b.ebms3.packaging;
+
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.soap.SOAPEnvelope;
+import org.apache.axiom.soap.SOAPHeaderBlock;
+import org.holodeckb2b.common.messagemodel.AgreementReference;
+import org.holodeckb2b.common.messagemodel.CollaborationInfo;
+import org.holodeckb2b.common.mmd.xml.MessageMetaData;
+import org.holodeckb2b.interfaces.general.EbMSConstants;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.xml.namespace.QName;
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created at 23:18 29.01.17
+ *
+ * @author Timur Shakuov (t.shakuov at gmail.com)
+ */
+public class AgreementRefElementTest {
+
+    private static final QName AGREEMENT_REF_INFO_ELEMENT_NAME =
+            new QName(EbMSConstants.EBMS3_NS_URI, "AgreementRef");
+
+    private OMElement umElement;
+    private OMElement ciElement;
+
+    @Before
+    public void setUp() throws Exception {
+        final String mmdPath =
+                this.getClass().getClassLoader()
+                        .getResource("packagetest/mmd_pcktest.xml").getPath();
+        final File f = new File(mmdPath);
+        MessageMetaData mmd = null;
+        try {
+            mmd = MessageMetaData.createFromFile(f);
+        } catch (final Exception e) {
+            fail("Unable to test because MMD could not be read correctly!");
+        }
+
+        // Creating SOAP envelope
+        SOAPEnvelope soapEnvelope =
+                SOAPEnv.createEnvelope(SOAPEnv.SOAPVersion.SOAP_12);
+        // Adding header
+        SOAPHeaderBlock headerBlock = Messaging.createElement(soapEnvelope);
+        // Adding UserMessage from mmd
+        umElement = UserMessageElement.createElement(headerBlock, mmd);
+
+        ciElement = CollaborationInfoElement.getElement(umElement);
+    }
+
+    @Test
+    public void testCreateElement() throws Exception {
+        AgreementReference agreementReference = new AgreementReference();
+        OMElement agreementRefElement =
+                AgreementRefElement.createElement(ciElement, agreementReference);
+        checkContainsInnerElements(agreementRefElement);
+    }
+
+    @Test
+    public void testGetElement() throws Exception {
+        OMElement agreementRefElement =
+                AgreementRefElement.getElement(ciElement);
+        checkContainsInnerElements(agreementRefElement);
+    }
+
+    @Test
+    public void testReadElement() throws Exception {
+        AgreementReference agreementReference = new AgreementReference();
+        String pmodeId = "some pmode id string";
+        agreementReference.setPModeId(pmodeId);
+        OMElement agreementRefElement =
+                AgreementRefElement.createElement(ciElement, agreementReference);
+
+        agreementReference =
+                AgreementRefElement.readElement(agreementRefElement);
+        checkContainsData(agreementReference, pmodeId);
+    }
+
+    private void checkContainsInnerElements(OMElement arElement) {
+        assertEquals(AGREEMENT_REF_INFO_ELEMENT_NAME, arElement.getQName());
+    }
+
+    private void checkContainsData(AgreementReference agreementReference,
+                                   String pmodeId) {
+        assertNotNull(agreementReference);
+        assertEquals(pmodeId, agreementReference.getPModeId());
+    }
+}
