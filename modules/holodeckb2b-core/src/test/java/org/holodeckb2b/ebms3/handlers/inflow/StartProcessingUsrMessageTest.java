7fadf66a348ed2b365420486b43c5f2cab7bcbf7
t.shakuov <t.shakuov@gmail.com>
2017-03-10 15:59:37 +0300

    Added one more handler junit test.

---
+++ modules/holodeckb2b-core/src/test/java/org/holodeckb2b/ebms3/handlers/inflow/StartProcessingUsrMessageTest.java
@@ -0,0 +1,137 @@
+
+/*
+ * Copyright (C) 2016 The Holodeck B2B Team, Sander Fieten
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.holodeckb2b.ebms3.handlers.inflow;
+
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.soap.SOAPEnvelope;
+import org.apache.axiom.soap.SOAPHeaderBlock;
+import org.apache.axis2.AxisFault;
+import org.apache.axis2.context.MessageContext;
+import org.apache.axis2.engine.Handler;
+import org.apache.log4j.Appender;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.spi.LoggingEvent;
+import org.holodeckb2b.common.messagemodel.UserMessage;
+import org.holodeckb2b.common.mmd.xml.MessageMetaData;
+import org.holodeckb2b.core.testhelpers.HolodeckB2BTestCore;
+import org.holodeckb2b.core.testhelpers.TestUtils;
+import org.holodeckb2b.ebms3.constants.MessageContextProperties;
+import org.holodeckb2b.ebms3.packaging.Messaging;
+import org.holodeckb2b.ebms3.packaging.SOAPEnv;
+import org.holodeckb2b.ebms3.packaging.UserMessageElement;
+import org.holodeckb2b.interfaces.core.HolodeckB2BCoreInterface;
+import org.holodeckb2b.interfaces.persistency.entities.IUserMessageEntity;
+import org.holodeckb2b.persistency.dao.StorageManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created at 22:19 09.03.17
+ *
+ * @author Timur Shakuov (t.shakuov at gmail.com)
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class StartProcessingUsrMessageTest {
+
+    @Mock
+    private Appender mockAppender;
+    @Captor
+    private ArgumentCaptor captorLoggingEvent;
+
+    private static String baseDir;
+
+    private static HolodeckB2BTestCore core;
+
+    private StartProcessingUsrMessage handler;
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        baseDir = StartProcessingUsrMessageTest.class.getClassLoader()
+                .getResource("handlers").getPath();
+        core = new HolodeckB2BTestCore(baseDir);
+        HolodeckB2BCoreInterface.setImplementation(core);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        handler = new StartProcessingUsrMessage();
+        LogManager.getRootLogger().addAppender(mockAppender);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        LogManager.getRootLogger().removeAppender(mockAppender);
+    }
+
+    @Test
+    public void testDoProcessing() throws Exception {
+        MessageMetaData mmd = TestUtils.getMMD("handlers/full_mmd.xml", this);
+        // Creating SOAP envelope
+        SOAPEnvelope env = SOAPEnv.createEnvelope(SOAPEnv.SOAPVersion.SOAP_12);
+        // Adding header
+        SOAPHeaderBlock headerBlock = Messaging.createElement(env);
+        // Adding UserMessage from mmd
+        OMElement umElement = UserMessageElement.createElement(headerBlock, mmd);
+
+        MessageContext mc = new MessageContext();
+        // Setting input message property
+        mc.setProperty(MessageContextProperties.IN_USER_MESSAGE, umElement);
+        try {
+            mc.setEnvelope(env);
+        } catch (AxisFault axisFault) {
+            fail(axisFault.getMessage());
+        }
+
+        UserMessage userMessage
+                = UserMessageElement.readElement(umElement);
+        String msgId = userMessage.getMessageId();
+        StorageManager updateManager = core.getStorageManager();
+        IUserMessageEntity userMessageEntity =
+                updateManager.storeIncomingMessageUnit(userMessage);
+        mc.setProperty(MessageContextProperties.IN_USER_MESSAGE,
+                userMessageEntity);
+
+        try {
+            Handler.InvocationResponse invokeResp = handler.invoke(mc);
+            assertNotNull(invokeResp);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
+        verify(mockAppender, atLeastOnce()).doAppend((LoggingEvent)captorLoggingEvent.capture());
+        LoggingEvent loggingEvent = (LoggingEvent)captorLoggingEvent.getValue();
+        //Check log level
+        assertThat(loggingEvent.getLevel(), is(Level.WARN));
+        //Check the message being logged
+        assertThat(loggingEvent.getRenderedMessage(),
+                is("User message [msgId= " + msgId + "] is ready for processing"));
+    }
+}
