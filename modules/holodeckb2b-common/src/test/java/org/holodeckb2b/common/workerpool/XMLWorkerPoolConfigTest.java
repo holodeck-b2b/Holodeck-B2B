2e73d427dd1d1d72917be79ec1b4d5f14caae7f1
t.shakuov <t.shakuov@gmail.com>
2017-05-12 14:53:47 +0300

    Fix of getting the resource path for Windows.
    Signed-off-by: t.shakuov <t.shakuov@gmail.com>

--- modules/holodeckb2b-common/src/test/java/org/holodeckb2b/common/workerpool/XMLWorkerPoolConfigTest.java
+++ modules/holodeckb2b-common/src/test/java/org/holodeckb2b/common/workerpool/XMLWorkerPoolConfigTest.java
@@ -87,7 +87,6 @@ public class XMLWorkerPoolConfigTest {
     @Test
     public void testLoadCorrectFile() {
         System.out.println("loadFromFile");
-//        final String path = this.getClass().getClassLoader().getResource("workerpoolcfg/wp_config1.xml").getPath();
         final String path = TestUtils.getPath(this.getClass(), "workerpoolcfg/wp_config1.xml");
 
         final IWorkerPoolConfiguration result = XMLWorkerPoolConfig.loadFromFile(path);
@@ -133,7 +132,6 @@ public class XMLWorkerPoolConfigTest {
     @Test
     public void testLoadInvalidFile() {
         System.out.println("loadFromFile");
-//        final String path = this.getClass().getClassLoader().getResource("workerpoolcfg/wp_config2.xml").getPath();
         final String path = TestUtils.getPath(this.getClass(), "workerpoolcfg/wp_config2.xml");
 
         final IWorkerPoolConfiguration result = XMLWorkerPoolConfig.loadFromFile(path);
@@ -160,7 +158,6 @@ public class XMLWorkerPoolConfigTest {
     @Test
     public void testLoadUnnamedFile() {
         System.out.println("loadFromFile");
-//        final String path = this.getClass().getClassLoader().getResource("workerpoolcfg/wp_config3.xml").getPath();
         final String path = TestUtils.getPath(this.getClass(), "workerpoolcfg/wp_config3.xml");
 
         final IWorkerPoolConfiguration result = XMLWorkerPoolConfig.loadFromFile(path);
