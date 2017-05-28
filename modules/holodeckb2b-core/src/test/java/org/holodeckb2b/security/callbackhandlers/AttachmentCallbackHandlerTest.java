983a200108b817575a5392d50908bd6d90f9060f
t.shakuov <t.shakuov@gmail.com>
2017-05-28 22:13:22 +0300

    PasswordCallbackHandlerTest added.

--- modules/holodeckb2b-core/src/test/java/org/holodeckb2b/security/callbackhandlers/AttachmentCallbackHandlerTest.java
+++ modules/holodeckb2b-core/src/test/java/org/holodeckb2b/security/callbackhandlers/AttachmentCallbackHandlerTest.java
@@ -18,11 +18,6 @@ package org.holodeckb2b.security.callbackhandlers;
 
 import org.apache.axiom.attachments.Attachments;
 import org.apache.axis2.context.MessageContext;
-import org.apache.log4j.Appender;
-import org.apache.log4j.Level;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-import org.apache.log4j.spi.LoggingEvent;
 import org.apache.wss4j.common.ext.Attachment;
 import org.apache.wss4j.common.ext.AttachmentRequestCallback;
 import org.apache.wss4j.common.ext.AttachmentResultCallback;
@@ -34,9 +29,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.Mock;
 
 import javax.activation.DataHandler;
 import javax.security.auth.callback.Callback;
@@ -53,12 +45,6 @@ import static org.mockito.Mockito.mock;
  * @author Timur Shakuov (t.shakuov at gmail.com)
  */
 public class AttachmentCallbackHandlerTest {
-
-    // Appender to control logging events
-    @Mock
-    private Appender mockAppender;
-    @Captor
-    private ArgumentCaptor<LoggingEvent> captorLoggingEvent;
 
     private static String baseDir;
 
@@ -80,15 +66,10 @@ public class AttachmentCallbackHandlerTest {
         mc = new MessageContext();
         setAttachments();
         handler = new AttachmentCallbackHandler(mc);
-        // Adding appender to the FindPModes logger
-        Logger logger = LogManager.getRootLogger();
-        logger.addAppender(mockAppender);
-        logger.setLevel(Level.DEBUG);
     }
 
     @After
     public void tearDown() throws Exception {
-        LogManager.getRootLogger().removeAppender(mockAppender);
         mc = null;
     }
 
