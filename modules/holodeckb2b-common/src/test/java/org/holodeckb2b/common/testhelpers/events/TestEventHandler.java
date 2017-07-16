ed725e70700d9b0c01f4af6dd8ec63ce092550ea
t.shakuov <t.shakuov@gmail.com>
2017-07-16 15:51:23 +0300

    Added necessary event handler classes.

---
+++ modules/holodeckb2b-common/src/test/java/org/holodeckb2b/common/testhelpers/events/TestEventHandler.java
@@ -0,0 +1,18 @@
+package org.holodeckb2b.common.testhelpers.events;
+
+import org.holodeckb2b.interfaces.events.IMessageProcessingEvent;
+import org.holodeckb2b.interfaces.events.IMessageProcessingEventHandler;
+
+/**
+ * Created at 16:37 15.07.17
+ *
+ * @author Timur Shakuov (t.shakuov at gmail.com)
+ */
+class TestEventHandler implements IMessageProcessingEventHandler {
+
+    @Override
+    public void handleEvent(IMessageProcessingEvent event)
+            throws IllegalArgumentException {
+
+    }
+}
