27e9323dfec486f6f39fba65a42dfe6dfc4c13ef
Sander Fieten <sander@holodeck-b2b.org>
2017-07-26 12:49:46 +0200

    Code clean up

--- modules/holodeckb2b-core/src/main/java/org/holodeckb2b/ebms3/handlers/inflow/HolodeckB2BDispatcher.java
+++ modules/holodeckb2b-core/src/main/java/org/holodeckb2b/ebms3/handlers/inflow/HolodeckB2BDispatcher.java
@@ -17,7 +17,6 @@
 package org.holodeckb2b.ebms3.handlers.inflow;
 
 import javax.xml.namespace.QName;
-
 import org.apache.axis2.AxisFault;
 import org.apache.axis2.context.MessageContext;
 import org.apache.axis2.description.AxisOperation;
@@ -35,14 +34,17 @@ public class HolodeckB2BDispatcher extends AbstractDispatcher
   public AxisOperation findOperation(final AxisService service, final MessageContext msgCtx)
                        throws AxisFault
   {
-    return service.getOperation(new QName("ebms"));
+    return service.getOperation(new QName("Receive"));
   }
 
   @Override
   public AxisService findService(final MessageContext msgCtx) throws AxisFault
   {
-
-    return msgCtx.getConfigurationContext().getAxisConfiguration().getService("msh");
+    AxisService axisService = msgCtx.getAxisService();
+    if (axisService != null)
+        return axisService;
+    else
+        return msgCtx.getConfigurationContext().getAxisConfiguration().getService("msh");
   }
 
   @Override
