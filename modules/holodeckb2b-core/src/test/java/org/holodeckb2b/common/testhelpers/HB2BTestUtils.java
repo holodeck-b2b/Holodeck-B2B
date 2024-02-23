c2c733a80995040a4b651488424247f085c77743
Sander Fieten <sander@holodeck-b2b.org>
2024-02-23 13:53:18 +0100

    Add 2-Way PModes for testing and stream equality test

--- modules/holodeckb2b-core/src/test/java/org/holodeckb2b/common/testhelpers/HB2BTestUtils.java
+++ modules/holodeckb2b-core/src/test/java/org/holodeckb2b/common/testhelpers/HB2BTestUtils.java
@@ -17,7 +17,10 @@
 package org.holodeckb2b.common.testhelpers;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
 import javax.xml.namespace.QName;
@@ -28,6 +31,7 @@ import org.holodeckb2b.common.pmode.PMode;
 import org.holodeckb2b.common.pmode.Protocol;
 import org.holodeckb2b.interfaces.general.EbMSConstants;
 import org.holodeckb2b.interfaces.general.IProperty;
+import org.holodeckb2b.interfaces.pmode.ILeg.Label;
 
 /**
  * Created at 15:22 19.02.17
@@ -36,34 +40,92 @@ import org.holodeckb2b.interfaces.general.IProperty;
  */
 public class HB2BTestUtils {
 
-    public static PMode create1WaySendPushPMode() {
+	public static PMode create1WaySendPushPMode() {
     	PMode pmode = new PMode();
     	pmode.setId(UUID.randomUUID().toString());
     	pmode.setMep(EbMSConstants.ONE_WAY_MEP);
     	pmode.setMepBinding(EbMSConstants.ONE_WAY_PUSH);
-    	
+
         Leg leg = new Leg();
         pmode.addLeg(leg);
-        
+
         Protocol prot = new Protocol();
         prot.setAddress("http://goes.no.where/msh");
         leg.setProtocol(prot);
-        
+
         return pmode;
     }
-    
-    public static PMode create1WayReceivePushPMode() {
+
+    public static PMode create1WayReceivePMode() {
     	PMode pmode = new PMode();
     	pmode.setId(UUID.randomUUID().toString());
     	pmode.setMep(EbMSConstants.ONE_WAY_MEP);
     	pmode.setMepBinding(EbMSConstants.ONE_WAY_PUSH);
-    	
+
     	Leg leg = new Leg();
     	pmode.addLeg(leg);
-    	
+
     	return pmode;
     }
-    
+
+	public static PMode create2WaySendOnReplyPMode() {
+    	PMode pmode = new PMode();
+    	pmode.setId(UUID.randomUUID().toString());
+    	pmode.setMep(EbMSConstants.TWO_WAY_MEP);
+    	pmode.setMepBinding(EbMSConstants.TWO_WAY_PUSH_PUSH);
+
+        Leg rcvleg = new Leg();
+        rcvleg.setLabel(Label.REQUEST);
+        pmode.addLeg(rcvleg);
+
+        Leg sendLeg = new Leg();
+        sendLeg.setLabel(Label.REPLY);
+        Protocol prot = new Protocol();
+        prot.setAddress("http://goes.no.where/msh");
+        sendLeg.setProtocol(prot);
+
+        pmode.addLeg(sendLeg);
+
+        return pmode;
+    }
+
+	public static PMode create2WaySendOnRequestPMode() {
+		PMode pmode = new PMode();
+		pmode.setId(UUID.randomUUID().toString());
+		pmode.setMep(EbMSConstants.TWO_WAY_MEP);
+		pmode.setMepBinding(EbMSConstants.TWO_WAY_PUSH_PUSH);
+
+		Leg sendLeg = new Leg();
+		sendLeg.setLabel(Label.REQUEST);
+		Protocol prot = new Protocol();
+		prot.setAddress("http://goes.no.where/msh");
+		sendLeg.setProtocol(prot);
+
+		pmode.addLeg(sendLeg);
+
+		Leg rcvleg = new Leg();
+		rcvleg.setLabel(Label.REPLY);
+		pmode.addLeg(rcvleg);
+
+		return pmode;
+	}
+
+
+    /**
+     * Asserts the content of the given streams is equal.
+     *
+     * @param is1
+     * @param is2
+     * @throws IOException
+     */
+    public static void assertEqual(InputStream is1, InputStream is2) throws IOException {
+		int b1, b2;
+		do {
+			b1 = is1.read(); b2 = is2.read();
+		} while (b1 == b2 && b1 >= 0 && b2 >= 0);
+		assertTrue(b1 < 0 && b2 < 0);
+    }
+
     /**
      *
      * @param elem
@@ -95,5 +157,6 @@ public class HB2BTestUtils {
     }
 
     private static final QName PROPERTY_ELEMENT_NAME =
-            new QName(EbMSConstants.EBMS3_NS_URI, "Property");    
+            new QName(EbMSConstants.EBMS3_NS_URI, "Property");
+
 }
