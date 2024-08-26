7cdb928f7f6088d19fa4b34574c5ec47e518a31d
Sander Fieten <sander@holodeck-b2b.org>
2024-08-26 12:39:06 +0200

    Use all payload meta-data in PSP

--- modules/holodeckb2b-interfaces/src/main/java/org/holodeckb2b/interfaces/storage/providers/IPayloadStorageProvider.java
+++ modules/holodeckb2b-interfaces/src/main/java/org/holodeckb2b/interfaces/storage/providers/IPayloadStorageProvider.java
@@ -17,37 +17,36 @@
 package org.holodeckb2b.interfaces.storage.providers;
 
 import org.holodeckb2b.interfaces.config.IConfiguration;
-import org.holodeckb2b.interfaces.messagemodel.Direction;
-import org.holodeckb2b.interfaces.pmode.IPMode;
 import org.holodeckb2b.interfaces.storage.IPayloadContent;
+import org.holodeckb2b.interfaces.storage.IPayloadEntity;
 
 /**
- * Defines the interface of a Holodeck B2B <i>Payload Storage Provider</i> that allows the Holodeck B2B Core to persist 
- * the payload content of processed message units. 
+ * Defines the interface of a Holodeck B2B <i>Payload Storage Provider</i> that allows the Holodeck B2B Core to persist
+ * the payload content of processed message units.
  * <p>
- * There can always be just one <i>Payload Storage Provider</i> active in an Holodeck B2B instance. The implementation 
- * to use is loaded using the Java <i>Service Prover Interface</i> mechanism. Therefore the JAR file containing the 
+ * There can always be just one <i>Payload Storage Provider</i> active in an Holodeck B2B instance. The implementation
+ * to use is loaded using the Java <i>Service Prover Interface</i> mechanism. Therefore the JAR file containing the
  * <i>Meta-data Storage Provider</i> implementation must contain a file in the <code>META-INF/services/</code> directory
- * named <code>org.holodeckb2b.interfaces.storage.providers.IPayloadStorageProvider</code> that contains the class name 
+ * named <code>org.holodeckb2b.interfaces.storage.providers.IPayloadStorageProvider</code> that contains the class name
  * of the provider implementation.
- * 
+ *
  * @author Sander Fieten (sander at holodeck-b2b.org)
- * @since 7.0.0 
+ * @since 7.0.0
  */
 public interface IPayloadStorageProvider {
 
     /**
-     * Gets the name of this provider to identify it in logging. This name is only used for logging purposes and it is 
-     * recommended to include a version number of the implementation. If no name is specified by the implementation the 
-     * class name will be used. 
+     * Gets the name of this provider to identify it in logging. This name is only used for logging purposes and it is
+     * recommended to include a version number of the implementation. If no name is specified by the implementation the
+     * class name will be used.
      *
      * @return  The name of the persistency provider.
      */
     default String getName() { return this.getClass().getName(); }
 
     /**
-     * Initialises the provider. This method is called once at startup of the Holodeck B2B instance. Since the message 
-     * processing depends on the correct functioning of the provider this method MUST ensure that all required 
+     * Initialises the provider. This method is called once at startup of the Holodeck B2B instance. Since the message
+     * processing depends on the correct functioning of the provider this method MUST ensure that all required
      * configuration and data is available.
      *
      * @param config	the Holodeck B2B configuration
@@ -55,45 +54,41 @@ public interface IPayloadStorageProvider {
      *                                  message SHOULD include a clear indication of what caused the init failure.
      */
 	void init(final IConfiguration config) throws StorageException;
-	
+
 	/**
-	 * Shuts down the provider. 
+	 * Shuts down the provider.
 	 * <p>This method is called by the Holodeck B2B Core when the instance is shut down. Implementations should use it
 	 * to release resources held for storing the message meta-data.
 	 */
 	void shutdown();
-	
+
 	/**
-	 * Creates a new {@link IPayloadContent} object that can be used to store the payload contents. The P-Mode may 
-	 * specify or provide specific parameters hw the payload content must be stored, for example encrypted. As the 
-	 * payloads of received and sent messages may need to be processed differently, also the direction of the User
-	 * Message in which the payload is contained must be specified.
-	 * 
-	 * @param payloadId	the <i>PayloadId</i> of the payload which content are to be stored
-	 * @param pmode		the P-Mode that governs the processing of the User Message the payload is contained in
-	 * @param direction	the direction of the User Message in which the payload is contained
-	 * @return {@link IPayloadContent} object to use for storing the payload content 
+	 * Creates a new {@link IPayloadContent} object that can be used to store the payload contents. The payload
+	 * meta-data, e.g. P-Mode governing processing of the containing User Message, may specify or provide specific
+	 * parameters how the payload content must be stored, for example encrypted.
+	 *
+	 * @param payloadInfo	meta-data of the payload which content are to be stored
+	 * @return {@link IPayloadContent} object to use for storing the payload content
 	 * @throws StorageException when an error occurs storing the payload content
 	 */
-	IPayloadContent createNewPayloadStorage(final String payloadId, final IPMode pmode, final Direction direction) 
-																								throws StorageException;
-	
+	IPayloadContent createNewPayloadStorage(final IPayloadEntity payloadInfo) throws StorageException;
+
 	/**
 	 * Retrieves the stored content of a payload.
-	 * 
-	 * @param payloadId	the <i>PayloadId</i> of the payload which content should be retrieved	
+	 *
+	 * @param payloadInfo	meta-data of the payload which content should be retrieved
 	 * @return {@link IPayloadContent} object to use for retrieving the payload content, or<br/>
-	 * 		   <code>null</code> if there is no content for the given <i>PayloadId</i> 
+	 * 		   <code>null</code> if there is no content for the given meta-data
 	 * @throws StorageException when an error occurs retrieving the payload content
 	 */
-	IPayloadContent getPayloadContent(final String payloadId) throws StorageException;
-	
+	IPayloadContent getPayloadContent(final IPayloadEntity payloadInfo) throws StorageException;
+
 	/**
 	 * Removes the stored content of a payload.
-	 * 
-	 * @param payloadId	the <i>PayloadId</i> of the payload which content should be removed
+	 *
+	 * @param payloadInfo	meta-data of the payload which content should be removed
 	 * @throws StorageException	when an error occurs that prevents the <i>Payload Storage Provider</i> to remove
-	 * 							the payload content. 
+	 * 							the payload content.
 	 */
-	void removePayloadContent(final String payloadId) throws StorageException;
+	void removePayloadContent(final IPayloadEntity payloadInfo) throws StorageException;
 }
