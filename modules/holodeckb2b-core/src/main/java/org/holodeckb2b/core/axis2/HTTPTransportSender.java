821336b327fbce8b3b291a744dfa5281cf667f03
Sander Fieten <sander@holodeck-b2b.org>
2025-12-23 13:08:58 +0100

    Add TLS client config to transport sender

--- modules/holodeckb2b-core/src/main/java/org/holodeckb2b/core/axis2/HTTPTransportSender.java
+++ modules/holodeckb2b-core/src/main/java/org/holodeckb2b/core/axis2/HTTPTransportSender.java
@@ -16,14 +16,19 @@
  */
 package org.holodeckb2b.core.axis2;
 
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
 import java.security.Security;
+import java.util.Iterator;
 
+import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
+import javax.xml.namespace.QName;
 
+import org.apache.axiom.om.OMElement;
 import org.apache.axis2.AxisFault;
 import org.apache.axis2.Constants;
 import org.apache.axis2.client.Options;
@@ -46,12 +51,16 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.jsse.provider.BouncyCastleJsseProvider;
+import org.holodeckb2b.commons.security.KeystoreUtils;
 import org.holodeckb2b.commons.util.Utils;
 import org.holodeckb2b.core.MessageProcessingContext;
 import org.holodeckb2b.core.pmode.PModeUtils;
+import org.holodeckb2b.interfaces.core.HolodeckB2BCoreInterface;
 import org.holodeckb2b.interfaces.messagemodel.IMessageUnit;
 import org.holodeckb2b.interfaces.pmode.ILeg;
 import org.holodeckb2b.interfaces.pmode.IProtocol;
+import org.holodeckb2b.interfaces.security.SecurityProcessingException;
+import org.holodeckb2b.interfaces.security.trust.ICertificateManager;
 
 /**
  * Is the Axis2 {@link HTTPTransportSender} implementation used for sending requests to other servers using HTTP. The
@@ -76,14 +85,23 @@ import org.holodeckb2b.interfaces.pmode.IProtocol;
  * <li>allowedECGroups : Can be used to specify the list of elliptic curve groups that are allowed to be used in the
  * 		TLS handshake. This is a generic setting that applies to all HTTPS connections. The value must be a comma
  * 		separated list of group names, in order of preference.</li>
+ * <li>CLIENT_TLS : should contain one or more <code>KeystoreAlias</code> elements that reference the keypair(s) managed
+ * 		by the <i>Certificate Manager</i> that by default should be used for TLS client authentication. The
+ * 		<code>password</code> attribute of these elements can contain the literal (= clear text) password or reference
+ * 		to a Java System Property or Environment Variable. In the latter cases the name of the property or environment
+ * 		variable should be prefixed with <i>sys:</i> respectively <i>env:</i>.</li>
  * </ul>
  * If the sender is invoked to send a message unit to another MSH, it will use the P-Mode governing the exchange of the
  * [primary] message unit to configure the HTTP(S) connection. The P-Mode can override the default settings specified in
  * the configuration of the <code>transportSender</code>.
  * <p>
- * <b>NOTE</b>: The sender does not determine the URL to which a request must be send. It is the responsibility of other
+ * <b>NOTES</b>:<br/>
+ * 1) The sender does not determine the URL to which a request must be send. It is the responsibility of other
  * components in the send chain to provide it in the {@link Constants.Configuration#TRANSPORT_URL} <code>MessageContext
- * </code> property.
+ * </code> property.<br/>
+ * 2) For backward compatibility the default Java system properties <i>javax.net.ssl.keyStore</i> and
+ * <i>javax.net.ssl.keyStorePassword</i> will be used to retrieve the key pair if the transport sender parameter is
+ * not provided. This method however is deprecated and will be removed in a future version!
  *
  * @author Sander Fieten (sander at holodeck-b2b.org)
  * @since 8.0.0
@@ -97,6 +115,8 @@ public class HTTPTransportSender extends HTTPClient4TransportSender {
 	 * when the message is sent.
 	 */
 	protected static final String MC_HTTP_REQUEST = "hb2b::httpRequest";
+
+	private static final char[] EMPTY_PWD = new char[] {};
 
 	/**
 	 * Indicates if the HTTP <i>"gzip"</i> compression content encoding should be used by default. When a message unit
@@ -146,7 +166,6 @@ public class HTTPTransportSender extends HTTPClient4TransportSender {
 			throw new AxisFault("Required BouncyCastle security providers could not be installed!");
 		}
 
-		log.trace("Prepare default SSLContext");
 		Parameter ecGroups = transportOut.getParameter("allowedECGroups");
 		if (ecGroups != null && ecGroups.getParameterType() == Parameter.TEXT_PARAMETER) {
 			System.setProperty("jdk.tls.namedGroups", (String) ecGroups.getValue());
@@ -154,15 +173,7 @@ public class HTTPTransportSender extends HTTPClient4TransportSender {
 		}
 		// Setup the default SSLContext to create TLS connections. If needed the P-Mode can override the TLS settings
 		// and a custom context will be used.
-		SSLContext sslContext;
-		try {
-			sslContext = SSLContext.getInstance("TLS", BouncyCastleJsseProvider.PROVIDER_NAME);
-			sslContext.init(null, new TrustManager[] {new TLSCertificateTrustManager()}, null);
-		} catch (NoSuchAlgorithmException | NoSuchProviderException | KeyManagementException e) {
-			log.error("Could not create default SSLContext: {}", Utils.getExceptionTrace(e));
-			throw new AxisFault("Could not create SSLContext", e);
-		}
-
+		SSLContext sslContext = createDefaultSSLContext(transportOut);
 		log.trace("Setup ConnectionManager");
 		@SuppressWarnings("deprecation")
 		Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
@@ -244,5 +255,82 @@ public class HTTPTransportSender extends HTTPClient4TransportSender {
 
 		// guard against multiple calls
 		msgContext.removeProperty(HTTPConstants.HTTP_METHOD);
+	}
+
+	/**
+	 * Creates a default SSLContext that will be used to create TLS connections if no specific TLS configuration is
+	 * provided in the P-Mode or this transport sender is invoked to send something in another context (e.g. to the
+	 * back-end).
+	 *
+	 * @param transportConfig 	transport sender configuration as provided in the Holodeck B2B config file
+	 * @return	the default SSL context
+	 * @throws AxisFault when there is an error creating the SSLContext, which can be caused by the fact that the
+	 * 					 specified key store is not available
+	 */
+	private SSLContext createDefaultSSLContext(TransportOutDescription transportConfig) throws AxisFault {
+		log.trace("Creating default SSLContext");
+		KeyStore tlsClientKs = null;
+		char[] tlsClientKsPwd = EMPTY_PWD;
+		Parameter aliasP = transportConfig.getParameter("CLIENT_TLS");
+		if (aliasP != null && aliasP.getParameterType() == Parameter.OM_PARAMETER) {
+			ICertificateManager cm = HolodeckB2BCoreInterface.getCertificateManager();
+			try {
+				tlsClientKs = KeyStore.getInstance(KeyStore.getDefaultType());
+				tlsClientKs.load(null, EMPTY_PWD);
+			} catch (Exception kse) {
+				log.fatal("Could not create TLS client cert key store : {}", Utils.getExceptionTrace(kse));
+				throw new AxisFault("Error constructing TLS client cert key store", kse);
+			}
+			for(Iterator<OMElement> ksElems = aliasP.getParameterElement().getChildrenWithLocalName("KeystoreAlias");
+				ksElems.hasNext();) {
+				OMElement ksElem = ksElems.next();
+				String alias = ksElem.getText();
+				String pwd = ksElem.getAttributeValue(new QName("password"));
+				if (pwd != null && pwd.startsWith("env:"))
+					pwd = System.getenv(pwd.substring(4));
+				else if (pwd != null && pwd.startsWith("sys:"))
+					pwd = System.getProperty(pwd.substring(4));
+				try {
+					tlsClientKs.setEntry(alias, cm.getKeyPair(alias, pwd), new KeyStore.PasswordProtection(EMPTY_PWD));
+					log.debug("Added {} as default TLS client cert", alias);
+				} catch (SecurityProcessingException | KeyStoreException kpNotAvailable) {
+					log.fatal("Could not load default TLS client cert {} : {}", alias,
+							Utils.getExceptionTrace(kpNotAvailable));
+					throw new AxisFault("Error in default TLS client cert config", kpNotAvailable);
+				}
+			}
+		} else {
+			String kspath = System.getProperty("javax.net.ssl.keyStore");
+			String kspwd = System.getProperty("javax.net.ssl.keyStorePassword");
+			if (!Utils.isNullOrEmpty(kspath)) {
+				log.warn("Deprecated client TLS configuration method used! This method may be removed in feature version!");
+				Path path = Paths.get(kspath);
+				if (!path.isAbsolute())
+					path = HolodeckB2BCoreInterface.getConfiguration().getHolodeckB2BHome().resolve(path);
+
+				try {
+					tlsClientKs = KeystoreUtils.load(path, kspwd);
+					tlsClientKsPwd = kspwd.toCharArray();
+					log.debug("Loaded default TLS client certs from {}", path.toString());
+				} catch (KeyStoreException ksNotAvailable) {
+					log.fatal("Could not load default TLS client certs from key store {} : {}", path.toString(),
+							Utils.getExceptionTrace(ksNotAvailable));
+					throw new AxisFault("Error in default TLS client cert config");
+				}
+			}
+		}
+		try {
+			log.trace("Construct KeyManager for default SSLContext");
+			KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+		    kmf.init(tlsClientKs, tlsClientKsPwd);
+		    SSLContext sslContext;
+			sslContext = SSLContext.getInstance("TLS", BouncyCastleJsseProvider.PROVIDER_NAME);
+			sslContext.init(kmf.getKeyManagers(), new TrustManager[] {new TLSCertificateTrustManager()}, null);
+			log.debug("Created default SSLContext");
+			return sslContext;
+		} catch (Exception ctxConstructionError) {
+			log.error("Could not create default SSLContext: {}", Utils.getExceptionTrace(ctxConstructionError));
+			throw new AxisFault("Could not create SSLContext", ctxConstructionError);
+		}
 	}
 }
